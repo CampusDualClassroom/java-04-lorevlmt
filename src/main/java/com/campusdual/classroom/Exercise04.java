19fa440f5e5a95155aeb33678cb313edbead1bb7
lorevlmt <lore.vimontes@gmail.com>
2024-10-28 16:24:16 +0100

    Ejercicio_04 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -10,21 +10,58 @@ public class Exercise04 {
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
 
+        System.out.println("Suma de los primeros 5 números: " + sumNum(5));
+
+        System.out.println("Suma de los primeros 20 números pares: " + sumEvenNum(20));
+
+        System.out.println("Obtén el factorial del número 7 → (7!): " + factorial(7));
+        System.out.println("Obtén el factorial del número 7 → (7!): " + recursiveFactorial(7));
     }
 
     public static int sumNum(int num) {
-        return 0;
+        int total = 0; //almacena la suma total
+
+        /* Hay que hacer un bucle que sume nº consecutivos
+           Se puede hacer con for y con while */
+        for (int i = 1; i <= num; i++) { // De 1 a 5 incluído
+            total = total + i;
+        }
+
+        return total;
     }
+
 
     public static int sumEvenNum(int num) {
-        return 0;
+        int total2 = 0;
+
+        for (int i = 2; i <= num * 2; i++) {
+            if (i % 2 == 0) {
+                total2 = total2 + i;
+            }
+        }
+
+        return total2;
     }
+
 
     public static int factorial(int num) {
-        return 0;
+        int total3 = 1;
+
+        for (int i = 1; i <= num; i++) {
+            total3 = total3 * i;
+        }
+
+        return total3;
     }
 
+
     public static int recursiveFactorial(int num) {
-        return 0;
+
+        if (num == 1) {
+            return 1;
+        }
+
+        return num * recursiveFactorial(num - 1);
+
     }
 }
